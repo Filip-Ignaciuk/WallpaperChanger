8c748685b5e2c4c109f39844188120ae030a92ab
Filip Ignaciuk <77020855+Filip-Ignaciuk@users.noreply.github.com>
2023-07-29 23:06:41 +0200

    Implemented issue #12
    Implemented issue #12 by moving all code into a static class that can be utilised with a hpp header and a cpp file.

---
+++ WallpaperChanger/WallpaperChanger/WallpaperChanger.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <iostream>
+#include <chrono>
+#include <string>
+#include <fstream>
+#include <Windows.h>
+#include <filesystem>
+#include <codecvt>
+#include <ShlObj.h>
+
+class WallpaperChanger
+{
+private:
+    static std::string ConvertWStrToStr(const std::wstring& wStr);
+
+    // Conversion of strings to wStrings.
+    static const std::wstring ConvertStrToWStr(const std::string& str);
+
+    // Converting all the \\ shlashes into one singular forward slash.
+    static const std::string NormaliseDir(std::string& str);
+
+    static const int GetCurrentDay();
+
+    static std::string GetDocumentDir();
+
+    static const char* daysLong[7];
+    static const std::string documentDir;
+    // In this case we can add / at the end of the directory as in all references of the variable it needs it.
+    static std::string documentDirCopy;
+    static const std::string imageDir;
+    static const int currentDay;
+
+    // Initialising files if they havent been created.
+    static void initFiles();
+
+
+    static void GetImages(std::filesystem::directory_entry _entry, std::vector<std::string>& _vector);
+
+
+public:
+    static void StartWallpaperChanger();
+
+};
