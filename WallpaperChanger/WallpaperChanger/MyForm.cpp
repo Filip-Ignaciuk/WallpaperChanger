ae8f7491b8d0ada526cc65f5ee48202562b2f505
Filip Ignaciuk <77020855+Filip-Ignaciuk@users.noreply.github.com>
2023-07-31 21:11:46 +0200

    Started work on issue #14
    Started work on issue #14, by porting the application to windows CLR.

---
+++ WallpaperChanger/WallpaperChanger/MyForm.cpp
@@ -0,0 +1,11 @@
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+void main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	WallpaperChanger::MyForm form;
+	Application::Run(% form);
+}
